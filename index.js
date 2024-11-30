ef4bb6afb650aec6736830ec48bdf3d8539f167e
Muiz13 <86073355+Muiz13@users.noreply.github.com>
2024-11-30 19:43:45 +0500

    Admin backend files

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+const express = require("express");
+const mongoose = require("mongoose")
+const app = express();
+app.use(express.json())
+require("dotenv").config();
+const cors = require("cors")
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const propertyRouter = require("./Routes/propertyRoutes");
+const mapRouter = require("./Routes/mapRoutes");
+const supplyRouter = require("./Routes/supplyRoutes");
+
+const userRouter = require("./Routes/userRoutes");
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use("/files",express.static('public/uploads'));
+
+app.use( '/admin',userRouter)
+app.use( '/property',propertyRouter)
+app.use( '/user', userRouter)
+app.use( '/map', mapRouter)
+app.use( '/supply', supplyRouter)
+
+
+// app.post("/upload",upload,(req,res)=>{
+//     console.log(req.body.file)
+//     res.send("Upload")
+// })
+
+
+app.listen(process.env.PORT||3005 , ()=>{
+    console.log(`App Listning at Port 3005`)
+})
+
+
+mongoose.connect(process.env.MOUNT_PAKISTAN_URI || MONGODB_URI).then(err=>{
+        console.log("Connected")
+})
